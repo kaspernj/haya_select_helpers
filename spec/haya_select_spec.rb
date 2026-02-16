063df687598b0da5d69418946fdb889f2bf5dbd0
kaspernj <kasper@diestoeckels.de>
2026-02-16 18:01:54 +0000

    Handle missing hidden input in value_no_wait

--- spec/haya_select_spec.rb
+++ spec/haya_select_spec.rb
@@ -1,6 +1,23 @@
 require "rails_helper"
 
 RSpec.describe HayaSelect do
+  describe "#value_no_wait" do
+    it "returns nil when the hidden input is missing" do
+      page = instance_double(Capybara::Session)
+      scope = instance_double("Scope", page: page)
+
+      expect(page).to receive(:first).with(
+        "[data-component='haya-select'][data-id='example'] [data-class='current-selected'] input[type='hidden']",
+        minimum: 0,
+        visible: false,
+        wait: 0
+      ).and_return(nil)
+
+      value = described_class.new(id: "example", scope: scope).send(:value_no_wait)
+      expect(value).to be_nil
+    end
+  end
+
   describe "#click_option_element" do
     it "ignores ElementNotInteractableError from location_once_scrolled_into_view" do
       scope = instance_double(Capybara::Session)
